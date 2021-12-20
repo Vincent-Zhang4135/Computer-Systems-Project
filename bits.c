e2784cb4d96694f64b1a9c4386c0e7fe36fd2de1
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-12-20 00:54:56 -0500

    Add files via upload

---
+++ bits.c
@@ -0,0 +1,493 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 
+ * bits.c - Source file with your solutions to the project.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solutiotn to the project by
+editing the collection of functions in this source file.
+
+CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. The maximum number of ops for each function is given in the
+     header comment for each function.
+
+/*
+ * STEP 2: Modify the following functions according to the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the btest checker to verify that your solutions produce
+ *      the correct answers.
+ */
+
+
+#endif
+
+/************************************************ 
+ * absVal - absolute value of x
+ *   Example: absVal(-1) = 1.
+ *   You may assume -TMax <= x <= TMax
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4
+ */
+int absVal(int x) {
+    int isNeg, negToPos;
+
+    isNeg = x >> 31;
+
+    // assuming x is negative, absolute value it:
+    negToPos = (~x + 1);
+
+    return (isNeg & negToPos) | (~isNeg & x); 
+}
+
+/************************************************ 
+ * addOK - Determine if can compute x+y without overflow
+ *   Example: addOK(0x80000000,0x80000000) = 0,
+ *            addOK(0x80000000,0x70000000) = 1, 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+int addOK(int x, int y) {
+    int x_sign, y_sign, sum_sign;
+
+    x_sign = (x >> 31) & 1;
+    y_sign = (y >> 31) & 1;
+    sum_sign = ((x + y) >> 31) & 1;
+
+    // int diff_sign = (x_sign ^ y_sign);
+    return !((x_sign ^ sum_sign) & (y_sign ^ sum_sign));
+}
+
+/************************************************ 
+ * allEvenBits - return 1 if all even-numbered bits in word set to 1
+ *   Examples allEvenBits(0xFFFFFFFE) = 0, allEvenBits(0x55555555) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int allEvenBits(int x) {
+    x = x & (x >> 16);
+    x = x & (x >> 8);
+    x = x & (x >> 4);
+    x = x & (x >> 2);
+    
+    return !!(x & 0x01);
+}
+
+/************************************************
+ * bang - Compute !x without using !
+ *   Examples: bang(3) = 0, bang(0) = 1
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 4 
+ */
+int bang(int x) {
+    x = x | x >> 16;
+    x = x | x >> 8;
+    x = x | x >> 4;
+    x = x | x >> 2;
+    x = x | x >> 1;
+
+    return (~x & 1);
+}
+
+/************************************************
+ * bitCount - returns count of number of 1's in word
+ *   Examples: bitCount(5) = 2, bitCount(7) = 3
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 40
+ *   Rating: 4
+ */
+int bitCount(int x) {
+    // four bit interval mask
+    int counts_a, counts_b, counts_c, counts_d, counts_e, counts_f, counts_g, counts_h;
+    int sum;
+    int mask = 0x1;
+    mask |= (mask << 8);
+    mask |= (mask << 16);
+
+    counts_a = x & mask;
+    counts_b = (x >> 1) & mask;
+    counts_c = (x >> 2) & mask;
+    counts_d = (x >> 3) & mask;
+    counts_e = (x >> 4) & mask;
+    counts_f = (x >> 5) & mask;
+    counts_g = (x >> 6) & mask;
+    counts_h = (x >> 7) & mask;
+
+    sum = counts_a + counts_b + counts_c + counts_d + counts_e + counts_f + counts_g + counts_h;
+
+    sum += ((sum >> 8) & 0xFF);
+    sum += ((sum >> 16) & 0xFF);
+    sum += ((sum >> 24) & 0xFF);
+
+    return sum & 0xFF;
+}
+
+/************************************************
+ * bitNor - ~(x|y) using only ~ and & 
+ *   Example: bitNor(0x6, 0x5) = 0xFFFFFFF8
+ *   Legal ops: ~ &
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitNor(int x, int y) {
+    return ((~x) & (~y));
+}
+
+/************************************************
+ * byteSwap - swaps the nth byte and the mth byte
+ *  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
+ *            byteSwap(0xDEADBEEF, 0, 2) = 0xDEEFBEAD
+ *  You may assume that 0 <= n <= 3, 0 <= m <= 3
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 25
+ *  Rating: 2
+ */
+int byteSwap(int x, int n, int m) {
+    int n_shifts;
+    int m_shifts;
+    int n_byte;
+    int m_byte;
+    int cleared;
+
+    n_shifts = n << 3;
+    m_shifts = m << 3;
+
+    n_byte = (x >> n_shifts) & 0xFF;
+    m_byte = (x >> m_shifts) & 0xFF;
+
+    cleared = x & ~(0xFF << n_shifts) & ~(0xFF << m_shifts);
+    return cleared | (n_byte << m_shifts) | (m_byte << n_shifts);
+}
+
+/************************************************ 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+    int condition = !!x;
+    // create an all 1 or all 0 bit number
+    condition = condition | condition << 1;
+    condition = condition | condition << 2;
+    condition = condition | condition << 4;
+    condition = condition | condition << 8;
+    condition = condition | condition << 16;
+
+    return (condition & y) | (~condition & z);
+}
+
+/************************************************
+ * ezThreeFourths - multiplies by 3/4 rounding toward 0,
+ *   Should exactly duplicate effect of C expression (x*3/4),
+ *   including overflow behavior.
+ *   Examples: ezThreeFourths(11) = 8
+ *             ezThreeFourths(-9) = -6
+ *             ezThreeFourths(1073741824) = -268435456 (overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 3
+ */
+int ezThreeFourths(int x) {
+    int isNeg; 
+    x += (x + x);
+    isNeg = x >> 31;
+
+    return ((x + ((4 + ~0x0) & isNeg)) >> 2);
+}
+
+/************************************************ 
+ * fitsBits - return 1 if x can be represented as an 
+ *  n-bit, two's complement integer.
+ *   1 <= n <= 32
+ *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int fitsBits(int x, int n) {
+    int shift = 33 + ~n; // translates to 33 + (- (n + 1))
+    return !(x ^ ((x << shift) >> shift));
+}
+
+/************************************************
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByte(0x12345678,1) = 0x56
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int getByte(int x, int n) {
+    int bits = n << 3;
+    return (x >> bits) & 0xFF;
+}
+
+/************************************************ 
+ * greatestBitPos - return a mask that marks the position of the
+ *               most significant 1 bit. If x == 0, return 0
+ *   Example: greatestBitPos(96) = 0x40
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 70
+ *   Rating: 4 
+ */
+int greatestBitPos(int x) {
+    int shifted_c;
+    int sig_bit;
+    // make everything after the most significant bit a one
+    x = x | (x >> 1);
+    x = x | (x >> 2);
+    x = x | (x >> 4);
+    x = x | (x >> 8);
+    x = x | (x >> 16);
+
+    shifted_c = (~x >> 1);
+    sig_bit = 0x1 << 31;
+
+    return (x & (shifted_c ^ sig_bit));
+}
+
+/************************************************
+ * implication - return x -> y in propositional logic - 0 for false, 1
+ * for true
+ *   Example: implication(1,1) = 1
+ *            implication(1,0) = 0
+ *   Legal ops: ! ~ ^ |
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int implication(int x, int y) {
+    return !!((!x) | y);
+}
+
+/************************************************ 
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
+ *            isAsciiDigit(0x05) = 0.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 3
+ */
+int isAsciiDigit(int x) {
+    int upperDiff;
+    int lowerDiff;
+    int isUpperNeg;
+    int isLowerNeg;
+
+    upperDiff = (0x39 + ~x + 1);
+    lowerDiff = (x + ~0x30 + 1);
+
+    isUpperNeg = upperDiff >> 31;
+    isLowerNeg = lowerDiff >> 31;
+
+    return !(isUpperNeg | isLowerNeg);
+}
+
+/************************************************ 
+ * isEqual - return 1 if x == y, and 0 otherwise 
+ *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
+ * 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int isEqual(int x, int y) {
+    return !~(~x ^ y);
+}
+
+/************************************************
+ * isLess - if x < y  then return 1, else return 0 
+ *   Example: isLess(4,5) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isLess(int x, int y) {
+    int diff, sign, negPos;
+    diff = (y + ~x) >> 31;
+    sign = (y ^ x) >> 31;
+    negPos = (x >> 31) & !(y >> 31);
+
+    return (!(diff | sign)) | negPos;
+}
+
+/************************************************
+ * isNonNegative - return 1 if x >= 0, return 0 otherwise 
+ *   Example: isNonNegative(-1) = 0.  isNonNegative(0) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 3
+ */
+int isNonNegative(int x) {
+    return !(x & (0x1 << 31));
+}
+
+/************************************************
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+    int isNonZero, isPos, isIntersection;
+
+    isNonZero = !!x;
+    isPos = !(x >> 31);
+    // if x is a power of 2, then x - 1 should have 0 overlap with x:
+    isIntersection = !((~1 + x + 1) & x);
+    return isNonZero & isPos & isIntersection;
+}
+
+/************************************************
+ * isTmin - returns 1 if x is the minimum, two's complement number,
+ *     and 0 otherwise 
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmin(int x) {
+    return !(!x + (x ^ (~x + 1)));
+}
+
+/************************************************
+ * minusOne - return a value of -1 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int minusOne(void) {
+    return ~0x0;
+}
+
+/************************************************
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateLeft(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >> !
+ *   Max ops: 25
+ *   Rating: 3 
+ */
+int rotateLeft(int x, int n) {
+    // int sig_bit = 0x1 << 31;
+    // int left_n_ones = sig_bit >> ~(1 + ~n); // ~(1 + ~n) translates to n - 1
+    int right_n_ones, left, right;
+    right_n_ones = ~(~0x0 << n);
+
+    //shift left part to the right
+    left = x << n;
+    //shift right part to the left
+    right = (x >> ~(n + ~32)) & right_n_ones;
+    return left | right;
+}
+
+/************************************************
+ * satMul2 - multiplies by 2, saturating to Tmin or Tmax if overflow
+ *   Examples: satMul2(0x30000000) = 0x60000000
+ *             satMul2(0x40000000) = 0x7FFFFFFF (saturate to TMax)
+ *             satMul2(0x60000000) = 0x80000000 (saturate to TMin)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+int satMul2(int x) {
+    return 2;
+}
---
+++ bits.h
@@ -0,0 +1,96 @@
+
+int absVal(int);
+int test_absVal(int);
+int addOK(int, int);
+int test_addOK(int, int);
+int allEvenBits();
+int test_allEvenBits();
+//#include "allOddBits.c"
+//#include "anyEvenBit.c"
+//#include "anyOddBit.c"
+int bang(int);
+int test_bang(int);
+//#include "bitAnd.c"
+int bitCount(int);
+int test_bitCount(int);
+//#include "bitMask.c"
+int bitNor(int, int);
+int test_bitNor(int, int);
+//#include "bitOr.c"
+//#include "bitParity.c"
+//#include "bitXor.c"
+int byteSwap(int, int, int);
+int test_byteSwap(int, int, int);
+int conditional(int, int, int);
+int test_conditional(int, int, int);
+//#include "copyLSB.c"
+//#include "divpwr2.c"
+//#include "evenBits.c"
+int ezThreeFourths(int);
+int test_ezThreeFourths(int);
+int fitsBits(int, int);
+int test_fitsBits(int, int);
+//#include "fitsShort.c"
+//#include "float_abs.c"
+//#include "float_f2i.c"
+//#include "float_half.c"
+//#include "float_i2f.c"
+//#include "float_neg.c"
+//#include "float_twice.c"
+int getByte(int, int);
+int test_getByte(int, int);
+int greatestBitPos(int);
+int test_greatestBitPos(int);
+//#include "howManyBits.c"
+//#include "ilog2.c"
+int implication(int, int);
+int test_implication(int, int);
+int isAsciiDigit(int);
+int test_isAsciiDigit(int);
+int isEqual(int, int);
+int test_isEqual(int, int);
+//#include "isGreater.c"
+int isLess(int, int);
+int test_isLess(int, int);
+//#include "isLessOrEqual.c"
+//#include "isNegative.c"
+int isNonNegative(int);
+int test_isNonNegative(int);
+//#include "isNonZero.c"
+//#include "isNotEqual.c"
+//#include "isPositive.c"
+int isPower2(int);
+int test_isPower2(int);
+//#include "isTmax.c"
+int isTmin(int);
+int test_isTmin(int);
+//#include "isZero.c"
+//#include "leastBitPos.c"
+//#include "leftBitCount.c"
+//#include "logicalNeg.c"
+//#include "logicalShift.c"
+int minusOne();
+int test_minusOne();
+//#include "multFiveEighths.c"
+//#include "negate.c"
+//#include "oddBits.c"
+//#include "rempwr2.c"
+//#include "replaceByte.c"
+//#include "reverseBytes.c"
+int rotateLeft(int, int);
+int test_rotateLeft(int, int);
+//#include "rotateRight.c"
+//#include "satAdd.c"
+int satMul2(int);
+int test_satMul2(int);
+//#include "satMul3.c"
+//#include "sign.c"
+//#include "sm2tc.c"
+//#include "subOK.c"
+//#include "tc2sm.c"
+//#include "thirdBits.c"
+//#include "tmax.c"
+//#include "tmin.c"
+//#include "trueFiveEighths.c"
+//#include "trueThreeFourths.c"
+//#include "upperBits.c"
